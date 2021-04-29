21c71325aa510d3547550eeb2e4ecfe8e00287ab
JamesDMSpeed <james.speed@ntnu.no>
2021-04-29 10:07:54 +0200

    SDM and KML

--- R scripts/HSM Dracocephalum ruyshiana.R
+++ R scripts/HSM Dracocephalum ruyshiana.R
@@ -7,6 +7,7 @@ library(sp)
 library(sdm)
 library(gridExtra)
 library(readxl)#Read in xls
+library(RColorBrewer)
 
 #Norway outline
 norway<-getData('GADM',level=0,country='NOR')
@@ -37,6 +38,15 @@ View(darcep)
 #Removing points without coordinates
 darcep<-darcep[!is.na(darcep$decimalLatitude) & !is.na(darcep$decimalLongitude),]
 
+#Removing points marked as outliers
+darcep<-darcep[is.na(darcep$Outliers),]
+
+#Removing fossil records
+summary(as.factor(darcep$basisOfRecord))
+darcep<-darcep[darcep$basisOfRecord!='FOSSIL_SPECIMEN',]
+
+dim(darcep)
+
 #Convert to spatial points dataframe
 darcep_sp<-SpatialPointsDataFrame(cbind(as.numeric(darcep$decimalLongitude),as.numeric(darcep$decimalLatitude)),darcep,proj4string=CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
 #Project to utm grid
@@ -52,23 +62,23 @@ darcep_simple<-data.frame(cbind(darcep_utm@coords,species=darcep$species))
 #Plots of env vars with occurrence records
 pt<-levelplot(predvars$bio10_16,scales=list(draw=F),margin=F,par.settings='YlOrRdTheme',main=expression('Mean summer temperature'~(degree~C)),
               colorkey=list(space='bottom',title=''))+
-  layer(sp.polygons(norwayP,col=grey(0.5)))+ 
-  layer(sp.points(darcep_utm,col=1,pch=16,cex=0.5))
+  latticeExtra::layer(sp.polygons(norwayP,col=grey(0.5)))+ 
+  latticeExtra::layer(sp.points(darcep_utm,col=1,pch=16,cex=0.5))
 
 pps<-levelplot(predvars$bio15_16,scales=list(draw=F),margin=F,par.settings='RdBuTheme',main='Precipitation seasonality',
                colorkey=list(space='bottom',title=expression('')))+
-  layer(sp.polygons(norwayP,col=grey(0.5)))+ 
-  layer(sp.points(darcep_utm,col=1,pch=16,cex=0.5))
+  latticeExtra::layer(sp.polygons(norwayP,col=grey(0.5)))+ 
+  latticeExtra::layer(sp.points(darcep_utm,col=1,pch=16,cex=0.5))
 
-pph<-levelplot(mask(predvars$SoilpH,predvars$bio10_16),scales=list(draw=F),margin=F,par.settings='RdBuTheme',main='Soil pH',
-               colorkey=list(space='bottom',title=expression('')))+
-  layer(sp.polygons(norwayP,col=grey(0.5)))+ 
-  layer(sp.points(darcep_utm,col=1,pch=16,cex=0.5))
+#pph<-levelplot(mask(predvars$SoilpH,predvars$bio10_16),scales=list(draw=F),margin=F,par.settings='RdBuTheme',main='Soil pH',
+#               colorkey=list(space='bottom',title=expression('')))+
+#  layer(sp.polygons(norwayP,col=grey(0.5)))+ 
+#  layer(sp.points(darcep_utm,col=1,pch=16,cex=0.5))
 
 pp<-levelplot(predvars$bio12_16,scales=list(draw=F),margin=F,par.settings='RdBuTheme',main=expression('Mean annual precipitation'~(mm)),
               colorkey=list(space='bottom'))+
-  layer(sp.polygons(norwayP,col=grey(0.5)))+ 
-  layer(sp.points(darcep_utm,col=1,pch=16,cex=0.5))
+  latticeExtra::layer(sp.polygons(norwayP,col=grey(0.5)))+ 
+  latticeExtra::layer(sp.points(darcep_utm,col=1,pch=16,cex=0.5))
 
 
 #Background data
@@ -77,13 +87,13 @@ background_dat<-read.table('Data/BackgroundBiasCorrected.txt',header=T)
 background_utm<-SpatialPoints(background_dat,proj4string=CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0'))
 
 pB<-levelplot(predvars[[1]]*0,col.regions=grey(1),colorkey=F,scales=list(draw=F),margin=F,par.settings='YlOrRdTheme',main='Background data')+
-  layer(sp.polygons(norwayP,col=grey(0.5)))+
-  layer(sp.points(background_utm,pch=1,cex=0.1,col=1))
+  latticeExtra::layer(sp.polygons(norwayP,col=grey(0.5)))+
+  latticeExtra::layer(sp.points(background_utm,pch=1,cex=0.1,col=1))
 pB
 
 tiff('Figures/AllData.tif',width = 1600,height = 1000,pointsize = 20,units = 'px')
 #pdf('Figures/AllData.pdf',width = 16,height = 10,pointsize = 20)
-grid.arrange(pt,pp,pps,pph,pB,ncol=3)
+grid.arrange(pt,pp,pps,ncol=3)
 #dev.off()
 dev.off()
 
@@ -137,8 +147,12 @@ sem<-function(x)sd(x,na.rm=T)/sqrt(length(!is.na(x)))
 varimp_mean<-with(VarImpdf,tapply(corTest,list(species,variables),mean))  
 varimp_sem<-with(VarImpdf,tapply(corTest,list(species,variables),sem))  
 
+varimp_mean
+varimp_sem
+
+
 tiff('Figures/VarImp.tif',width=800,height=600,pointsize = 20)
-pdf('Figures/VarImp.pdf',width=9,height=7,pointsize = 20)
+#pdf('Figures/VarImp.pdf',width=9,height=7,pointsize = 20)
 par(mar=c(5,5,1,1))
 b1<-barplot(varimp_mean,beside=T,ylab='Variable importance',names.arg=c('MST','MAP','Precipitation \n seasonality'),las=1,ylim=c(0,0.80),
             legend.text=sub("_"," ",rownames(varimp_mean)))     
@@ -154,9 +168,9 @@ for (i in 1:length(levels(as.factor(sdm1@run.info$species)))){
                                            ,mean=T,main=levels(as.factor(sdm1@run.info$species))[i])
 }
 
-tiff('Figures/ResponseCurves.tif',width = 1200,height=1200,units='px',pointsize = 20)
-pdf('Figures/ResponseCurves.pdf',width = 13,height=13,pointsize = 20)
-par(mfrow=c(3,1))
+tiff('Figures/ResponseCurves.tif',width = 1200,height=800,units='px',pointsize = 20)
+#pdf('Figures/ResponseCurves.pdf',width = 13,height=13,pointsize = 20)
+par(mfrow=c(1,3))
 par(mar=c(5,5,1,1))
 plot(responsecurvelist[[1]]@response$bio10_16[,1],apply(responsecurvelist[[1]]@response$bio10_16[,2:ncol(responsecurvelist[[1]]@response$bio10_16)],1,mean)
      ,type='l',xlab=expression('MST'~(degree~C)),ylab='Response',las=1,ylim=c(0,1))
@@ -189,28 +203,33 @@ drapred<-predict(sdm1,predvars,filename='Outputs/Model Predictions/dracep',
                  species='Dracocephalum_ruyshiana',mean=T,overwrite=T)
 drapred  
 drapred_mean<-calc(drapred,mean)
-levelplot(drapred_mean,scales=list(draw=F),names.attr=c('C. lepidocarpa','C. jemtlandica'))
+levelplot(drapred_mean,scales=list(draw=F),margin=F)
 
 #Ensembling
 ens_dracep<-ensemble(sdm1,newdata = predvars,filename = 'Outputs/Model Predictions/dracep_ensemble',
-                  setting=list(method='weighted',stat='AUC',overwrite=T,
-                               id=sdm1@run.info$modelID),overwrite=TRUE)
-ens_dracep<-raster('ModelPredictions/dracep_ensemble')
+                  setting=list(method='weighted',stat='AUC'),overwrite=T)
+                           
+ens_dracep<-raster('Outputs/Model Predictions/dracep_ensemble')
 
 tiff('Figures/HSMMap.tif',width=1200,height=800,units='px',pointsize = 20,res=300)
-pdf('Figures/HSMMap.pdf',width=1200,height=800,pointsize = 20)
-levelplot(ens_dracep,scales=list(draw=F),par.settings='YlOrRdTheme',
-          cex=1.5)+
-  layer(sp.polygons(norwayP,col=grey(0.5)))
+#pdf('Figures/HSMMap.pdf',width=1200,height=800,pointsize = 20)
+levelplot(ens_dracep,scales=list(draw=F),margin=F,par.settings='YlOrRdTheme')+
+  latticeExtra::layer(sp.polygons(norwayP,col=grey(0.5),lwd=0.02))+
+  latticeExtra::layer(sp.points(darcep_utm,pch=1,cex=0.1,col=1))
 dev.off()
 dev.off()
 
+kmlcols<- brewer.pal(9, 'YlOrRd')
+KML(projectRaster(ens_dracep,res=0.008333333,crs=norway@proj4string),
+    'Outputs/Dracocephalum_ruyschiana_climatesuitabilitymodel',overwrite=T,
+    col=kmlcols)
+
 #Niche (MST and precip season)
-niche_temp_pc_dracep<-niche(predvars,ens_dracep,c('bio10_16','bio15_16'),plot=F)
+niche_temp_pc_dracep<-sdm::niche(predvars,ens_dracep,c('bio10_16','bio15_16'),plot=F)
 
 #Stack up niches
 
-levelplot(nniche_temp_pc_dracep,par.settings='YlOrRdTheme')
+levelplot(niche_temp_pc_dracep,par.settings='YlOrRdTheme')
 #Set extent as actual climate variables
 extent(niche_temp_pc_dracep)<-stack(niche_temp_pc_dracep@scaleParams)[,1]
 tiff('Figures/Niches.tif',width=800,height = 800,res=150)
@@ -221,9 +240,9 @@ levelplot(niche_temp_pc_dracep,par.settings='YlOrRdTheme',
 dev.off()
 dev.off()
 
-niche_temp_map_dracep<-niche(predvars,ens_dracep,c('bio10_16','bio12_16'),plot=F)
-
-levelplot(niche_temp_map_dracep,par.settings='YlOrRdTheme')
+niche_temp_map_dracep<-sdm::niche(predvars,ens_dracep,c('bio10_16','bio12_16'),plot=F)
+plot(niche_temp_map_dracep)
+levelplot(raster(niche_temp_map_dracep),par.settings='YlOrRdTheme')
 #Set extent as actual climate variables
 extent(niche_temp_map_dracep)<-stack(niche_temp_map_dracep@scaleParams)[,1]
 levelplot(niche_temp_map_dracep,par.settings='YlOrRdTheme',
